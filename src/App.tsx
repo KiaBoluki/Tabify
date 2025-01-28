7b60a4079df6be579982379ce7b823ef1f58de4e
Kia Boluki <kia.boluki@gmail.com>
2025-01-28 20:55:54 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
