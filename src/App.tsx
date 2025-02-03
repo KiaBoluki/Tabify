6385760ffb118bcd0507ec5eb6799e44d64b9f95
Kia Boluki <kia.boluki@gmail.com>
2025-02-03 05:07:21 +0400

    initiate date time component to show the persian and gregorian date and time

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import Card from './components/Card'
-import { PersianShortDate } from './components/DateTime'
+import "./App.css";
+import Card from "./components/Card";
+import DateTime, { PersianDate, PersianTime } from "./components/DateTime";
 
 function App() {
   return (
     <div className="wrapper text-white bg-slate-300 min-h-screen flex justify-center items-center">
       <Card>
-         <PersianShortDate/>
+        <div className="flex items-center justify-center">
+          <DateTime/>
+        </div>
+        <div className="flex items-center justify-between">
+          <PersianTime />
+          <PersianDate />
+        </div>
       </Card>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
