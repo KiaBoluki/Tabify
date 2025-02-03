ed8859a0e3caae6cc81e0066e9a769a9eae1a673
Kia Boluki <kia.boluki@gmail.com>
2025-02-03 05:08:07 +0400

    create qoute component

---
+++ src/components/Qoute.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const Qoute = () => {
+    const [Qoute, setQoute] = useState('');
+    
+    useEffect(() => {
+        const qoute = async () => {
+            
+        }
+    }); 
+
+}
+
+
+export default Qoute; 
