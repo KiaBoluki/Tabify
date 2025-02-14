9d56e27e6e492e0cd465cd47bd120ff6eb695803
Kia Boluki <kia.boluki@gmail.com>
2025-02-14 17:53:27 +0400

    set and get greeting message from localStorage

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,15 +1,21 @@
-const greetingElement = document.getElementById('greeting')
-const submitElement = document.getElementById('submit')
+const greetingInput = document.getElementById("greeting");
+const greetingMessage = localStorage.getItem("tabify-greeting-message") ?? "hello"; 
 
-chrome.storage.sync.get(['greetingMessage'], result => {
-  if (result) {
-    greetingElement.placeholder = result.greetingMessage;
+const loadGreeting = () => {
+  return localStorage.getItem("tabify-greeting-message"); 
+}
+
+greetingInput.addEventListener('blur', (event) => {
+  const value = event.target.value; 
+  if ( value ){
+    localStorage.setItem("tabify-greeting-message", value);
   }
 })
 
-submitElement.addEventListener('click', () => {
-  chrome.storage.sync.set({ greetingMessage: greetingElement.value }, () => {
-    console.log('اطلاعات ذخیره شد');
-  });
-})
+const loadOptions = () => {
+   greetingInput.value = loadGreeting(); 
+}
 
+document.addEventListener('DOMContentLoaded', ()=>{
+  loadOptions(); 
+})
