42d6037031297e8fffa5f298af927e9b5a5612d7
Kia Boluki <kia.boluki@gmail.com>
2025-02-17 18:37:23 +0400

    add a limit of 16 character for greeting message

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,14 +1,17 @@
 const greetingMessageInput = document.getElementById("greeting-message-input");
 const greetingMessage = localStorage.getItem("greeting-message") ?? "hello";
-const greetingMessageSwitchEl = document.getElementById("greeting-message-switch"); 
-const wiseQouteSwitchEl = document.getElementById("wise-qoute-switch"); 
+const greetingMessageSwitchEl = document.getElementById(
+  "greeting-message-switch"
+);
+const wiseQouteSwitchEl = document.getElementById("wise-qoute-switch");
 
 const loadGreeting = () => {
-  return localStorage.getItem("greeting-message"); 
-}
+  return localStorage.getItem("greeting-message");
+};
 
 const saveGreeting = debounce((value) => {
   value = value.trim();
+  value = value.length > 16 ? value.substring(0, 16) : value;
   if (isValidate(value)) {
     localStorage.setItem("greeting-message", value);
   }
@@ -27,28 +30,30 @@ function debounce(func, wait) {
 }
 
 const loadOptions = () => {
-   greetingMessageInput.value = loadGreeting(); 
-   greetingMessageSwitchEl.checked = JSON.parse(localStorage.getItem('show-greeting-message')); 
-   wiseQouteSwitchEl.checked = JSON.parse(localStorage.getItem('show-wise-qoute')); 
-}
+  greetingMessageInput.value = loadGreeting();
+  greetingMessageSwitchEl.checked = JSON.parse(
+    localStorage.getItem("show-greeting-message")
+  );
+  wiseQouteSwitchEl.checked = JSON.parse(
+    localStorage.getItem("show-wise-qoute")
+  );
+};
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  loadOptions(); 
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  loadOptions();
+});
 
 const isValidate = (value) => {
- return value !== undefined && value !== null;
-}
+  return value !== undefined && value !== null;
+};
 
-greetingMessageSwitchEl.addEventListener('change', (event)=>{
+greetingMessageSwitchEl.addEventListener("change", (event) => {
   console.log(event.target.checked);
   // Enable the input if the switch is checked, otherwise disable it
   greetingMessageInput.disabled = !event.target.checked;
-  localStorage.setItem('show-greeting-message', event.target.checked)
-
-})
-wiseQouteSwitchEl.addEventListener('change', (event)=>{
+  localStorage.setItem("show-greeting-message", event.target.checked);
+});
+wiseQouteSwitchEl.addEventListener("change", (event) => {
   console.log(event.target.checked);
-  localStorage.setItem('show-wise-qoute', event.target.checked)
-})
+  localStorage.setItem("show-wise-qoute", event.target.checked);
+});
