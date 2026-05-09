c98a90a1a0b2502d2c63ba4ec61e62f152353bab
KiaBoluki <kia.boluki@gmail.com>
2026-05-09 06:25:24 +0400

    Refactor Vite configuration for improved readability and consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      "/api/usd": {
-        target: "https://alanchand.com",
+      '/api/usd': {
+        target: 'https://alanchand.com',
         changeOrigin: true,
-        rewrite: () => "/currencies-price/usd",
+        rewrite: () => '/currencies-price/usd',
+      },
+      '/api/quote': {
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+        rewrite: () => '/quotes/random',
       },
     },
   },
-});
+})
